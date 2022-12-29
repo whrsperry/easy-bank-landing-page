2724ffc885b4d9017f8529ce861e84497bc65961
whrsperry <imbuidoperry@gmail.com>
2022-12-30 03:09:54 +0800

    feat: adding a functionality on hamburger menu

---
+++ app/js/script.js
@@ -0,0 +1,11 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+
+btnHamburger.addEventListener('click', function() {
+  
+  if (header.classList.contains('open')) {
+    header.classList.remove('open');
+  } else {
+    header.classList.add('open');
+  }
+});
