c18e480866d8510f621dac280c6bfbb8969a48b3
whrsperry <imbuidoperry@gmail.com>
2022-12-30 14:12:42 +0800

    feat: adding menu toggle on the hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,27 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 
 btnHamburger.addEventListener('click', function() {
   
   if (header.classList.contains('open')) { // Close  Hamburger Menu
+    body.classList.remove('noscroll');
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    fadeElements.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    })
+    
   } 
   else { // Open Hamburger Menu
+    body.classList.add('noscroll');
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    fadeElements.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    })
+    
   }
 });
